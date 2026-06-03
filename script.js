3c5b3ff367f25212f411d3f886210c8f9c0a929d
servant of god <98dppdtxh2@privaterelay.appleid.com>
2026-06-04 02:44:21 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let localReturnPage = 'prayers';
+let lastScrollPosition = 0; // აქ შევინახავთ სკროლის ადგილს
 
 function openSagalobeli(fromPageId) {
     localReturnPage = fromPageId; 
-    // ვცადოთ page-ს გარეშე, რადგან showPage-ს შეიძლება ასე უნდა
+    lastScrollPosition = window.scrollY; // ვიმახსოვრებთ ზუსტად სად იდგა მომხმარებელი
+    
     showPage('prayer-ghirs-natsv'); 
 }
 
 function goBackToWhereIWas() {
     showPage(localReturnPage); 
+    
+    // პატარა თაიმაუთი გვჭირდება, რომ გვერდმა ჯერ ჩატვირთვა მოასწროს და მერე ჩამოსკროლოს
+    setTimeout(() => {
+        window.scrollTo(0, lastScrollPosition); // აბრუნებს მომხმარებელს ზუსტად იმავე ადგილას
+    }, 50); 
 }
